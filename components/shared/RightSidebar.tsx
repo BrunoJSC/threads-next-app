b41a656fb0c9143b519ea64a557ec1c63cb6a129
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-08-20 01:58:32 -0300

    first commit

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,17 @@
+function RightSidebar() {
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <div className="text-heading4-medium text-light-1">
+          Suggested Community
+        </div>
+      </div>
+
+      <div className="flex flex-1 flex-col justify-start">
+        <div className="text-heading4-medium text-light-1">Suggested Users</div>
+      </div>
+    </section>
+  );
+}
+
+export default RightSidebar;
